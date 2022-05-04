7adc6927ecb04d8d8080753bc6b9b9a2879817ec
Alexander Ageyev <s94827@yandex.ru>
2022-05-04 21:36:08 +0500

    debugging

--- Program.cs
+++ Program.cs
@@ -84,6 +84,8 @@ namespace Plants
 
             #endregion
 
+            FreeLibSet.Forms.Diagnostics.DebugTools.DebugFocusWindowVisible = true;
+
             spl.Complete();
 
             PlanReport.ShowOnStart();

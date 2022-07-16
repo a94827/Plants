7afd8ce2e4a18847eb7f957552433414b285a16b
Alexander Ageyev <s94827@yandex.ru>
2022-07-16 11:14:36 +0500

    v20

--- __save.bat
+++ __save.bat
@@ -3,7 +3,7 @@ if exist src.7z del src.7z
 7za.exe a src.7z History.htm
 
 if exist exe.7z del exe.7z
-cd BIN\x86\Debug
-7za.exe a -r ..\..\..\exe.7z Plants.exe *.dll
-cd ..\..\..
+cd BIN\Release
+7za.exe a -r ..\..\exe.7z Plants.exe *.dll
+cd ..\..
 
